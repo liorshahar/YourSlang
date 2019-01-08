faf02ddedd19b599af373f3f4d1a45e635c56756
liorsh <liorsh@shenkar.ac.il>
2019-01-08 23:53:13 +0200

    adding tv Show by name routes

--- FrontEndSide/include/js/googlecharts.js
+++ FrontEndSide/include/js/googlecharts.js
@@ -22,4 +22,32 @@ $(window).on("load", function() {
     );
     chart.draw(data, google.charts.Bar.convertOptions(options));
   }
+
+  $(".col-lg-3.tvShow").click(function(event) {
+    let parentDiv = event.target.parentNode;
+    let tvShowName = parentDiv.children[1].innerText;
+    console.log("Tv Show Name: " + tvShowName);
+    drawChartByName(tvShowName);
+  });
 });
+
+function drawChartByName(name) {
+  var jsonData = $.ajax({
+    url: "http://localhost:3004/getTvShowByNameRoutes/" + name,
+    dataType: "json", // type of data we're expecting from server
+    async: false // make true to avoid waiting for the request to be complete
+  });
+  console.log(jsonData.responseJSON);
+  var data = google.visualization.arrayToDataTable(jsonData.responseJSON);
+
+  var options = {
+    chart: {
+      title: "כמות ציוצים לתוכנית"
+    },
+    bars: "horizontal" // Required for Material Bar Charts.
+  };
+  var chart = new google.charts.Bar(
+    document.getElementById("barchart_material")
+  );
+  chart.draw(data, google.charts.Bar.convertOptions(options));
+}
